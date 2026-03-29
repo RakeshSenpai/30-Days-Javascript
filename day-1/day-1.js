e0d7962cb7126adf9a2896f6589e58862ce584f8
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-29 23:27:18 +0530

    relationships of DOM

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,4 +1,10 @@
 const parent = document.querySelector('.parent')
-console.log(parent)
-console.log(parent.children)
-console.log(parent.children[0])
+// console.log(parent)
+// console.log(parent.children)
+// console.log(parent.children[0])
+
+for (let i = 0; i < parent.children.length; i++){
+    console.log(parent.children[i].innerHTML)
+}
+
+parent.children[2].style.color = 'grey'
