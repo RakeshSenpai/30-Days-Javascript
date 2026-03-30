f413be47ad27253a8ba92bfe24b9dfa5647386cb
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-30 22:14:43 +0530

    day-2

--- Day-2/day-2.js
+++ Day-2/day-2.js
@@ -1,11 +1,17 @@
 const gParent = document.querySelector('.grandparent')
-const parent = Array.from(document.getElementsByClassName('parent'))
-const child = document.querySelector('.child')
+// const parent = Array.from(document.getElementsByClassName('parent'))
+// const child = document.querySelector('.child')
 
+const parents = Array.from(gParent.children)
+const childrenOne = parents[0]
+const children = childrenOne.children
+
+changeColor(children[0])
 
 // changeColor(gParent)
-parent.forEach((e) => {changeColor(e)})
+// parent.forEach((e) => {changeColor(e)})
 
 function changeColor(e){
     e.style.backgroundColor = '#03cbf4'
 }
+
