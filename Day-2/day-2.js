335f18dbd937d3a20b3f831bccacc0cc3a780694
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-30 23:14:02 +0530

    events

--- Day-2/day-2.js
+++ Day-2/day-2.js
@@ -1,17 +1,16 @@
-const gParent = document.querySelector('.grandparent')
+// const gParent = document.querySelector('.grandparent')
 // const parent = Array.from(document.getElementsByClassName('parent'))
 // const child = document.querySelector('.child')
 
-const parents = Array.from(gParent.children)
-const childrenOne = parents[0]
-const children = childrenOne.children
+// const parents = Array.from(gParent.children)
+// const childrenOne = parents[0]
+// const children = childrenOne.children
 
-changeColor(children[0])
+// changeColor(children[0])
 
 // changeColor(gParent)
 // parent.forEach((e) => {changeColor(e)})
 
-function changeColor(e){
-    e.style.backgroundColor = '#03cbf4'
-}
-
+// function changeColor(e){
+//     e.style.backgroundColor = '#03cbf4'
+// }
---
+++ Day-2/events.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('clBtn')
+const div = document.querySelector('.container')
+const li = document.querySelector('li')
+
+// div.addEventListener('click' , (e) => {
+//     console.log('the container clicked')
+//     e.stopPropagation()
+// }, false)
+// li.addEventListener('click' , (e) => {
+//     console.log('the Li clicked')
+//     e.stopPropagation()
+// }, false)
+
+// btn.addEventListener('click' , (e) => {
+//     console.log(e)
+// })
+
+btn.addEventListener('click', (e)=> {
+    const newItem = document.createElement('h3')
+    newItem.innerText = 'Hi i am new here'
+    div.appendChild(newItem)
+})
