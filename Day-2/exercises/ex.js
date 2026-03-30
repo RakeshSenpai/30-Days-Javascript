f263b36139e9a67ae379e2486f20c02db83fbd07
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-31 00:49:53 +0530

    validate form

--- Day-2/exercises/ex.js
+++ Day-2/exercises/ex.js
@@ -1,3 +1,5 @@
+/*
+
 const btn = document.getElementById('changeColor')
 const box = document.querySelector('.box')
 
@@ -9,3 +11,30 @@ function changeColor(){
 btn.addEventListener('click' , (e) => {
     changeColor()
 })
+
+*/
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault()
+        const data = new FormData(form)
+        const username = data.get('username')
+        const password = data.get('password')
+        if(username === '' || password === ''){
+            alert('Please complete your form')
+            return;
+        }
+
+        const firstDiv = document.createElement('div')
+        const secondDiv = document.createElement('div')
+        const validate = document.createElement('div')
+        validate.innerText = '✅'
+        firstDiv.innerText = username
+        secondDiv.innerText = password
+        form.appendChild(firstDiv)
+        form.appendChild(secondDiv)
+        form.appendChild(validate)
+
+})
+
