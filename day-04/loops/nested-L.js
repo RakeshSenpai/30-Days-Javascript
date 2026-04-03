be4827a90e5cbd1408f189702af486c136974dbd
Rakesh07778777 <bossrakesh43@gmail.com>
2026-04-03 20:11:54 +0530

    nested-loops

--- day-04/loops/nested-L.js
+++ day-04/loops/nested-L.js
@@ -1,6 +1,14 @@
-  for (let i = 0; i < 3 ; i++){
-    // console.log('outer Loop' + i)
-    for(let j = 0 ; j < i; j++){
-        console.log(('outter Loop' + i), ('inner Loop' + j))
+  // for (let i = 0; i < 3 ; i++){
+  //   console.log('outer Loop' + i)
+  //   for(let j = 0 ; j < i; j++){
+  //       console.log(('outter Loop' + i), ('inner Loop' + j))
+  //   }
+  // }
+
+  for(let i = 0; i < 4; i++){
+    let row = ''
+    for(let j = 0; j < 7; j++){
+      row += '☀️'
     }
+    console.log(row)
   }
